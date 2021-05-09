36d0b4f51ac6469148ce9f5032d1b84dc3df6193
Sime Demo <sdemo@pmfst.hr>
2021-05-09 23:55:41 +0200

    new file:   ../../Domaci/Domaci_3/harmonicos.cpp

---
+++ Domaci/Domaci_3/harmonicos.cpp
@@ -0,0 +1,62 @@
+#include <iostream>
+#include <math.h>
+#include <vector>
+using namespace std;
+
+class HarmonicOscillator {
+
+    public:
+        float m;
+        float k;
+        float x0;
+        vector<float> x;
+        float v0;
+        vector<float> v;
+        vector<float> a;
+        vector<float> t_list;
+        float t;
+
+        HarmonicOscillator(float _m, float _k, float _x0, float _v0, float _t) {
+
+            m = _m;
+            k = _k;
+            x.push_back(_x0);
+            v.push_back(_v0);
+            a.push_back((-_k * _m) * x.back());
+            t = _t;
+            t_list.push_back(0);
+
+        }
+    
+    private:
+
+        void __reset() {  // for plotting
+            x.clear();
+            x.push_back(x0);
+            v.clear();
+            v.push_back(v0);
+            a.clear();
+            a.push_back((k * m) * x.back());
+            t_list.clear();
+            t_list.push_back(0);
+        }
+
+    public:
+
+        void oscillate(int dt) {
+
+            for (int i = 0; i < (t / dt); i++) {
+                t_list.push_back(t_list.back() * dt);
+                a.push_back((-k / m) * x.back());
+                v.push_back(v.back() + (a.back() * dt));
+                x.push_back(x.back() + (v.back() * dt));
+            }
+        }
+    
+};
+
+int main() {
+
+    HarmonicOscillator HarmonicOscillator (10, 5, 5, 5, 50);
+    HarmonicOscillator.oscillate(0.01);
+}
